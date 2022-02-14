a903b9823213c2072838ff24fff1a571130504ff
Vinicius Coutinho <vini.coutinho.if@gmail.com>
2022-02-13 21:00:27 -0300

    Infra.Data: Product and Category ModelBuilder configuration

---
+++ CleanArchProject.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using CleanArchProject.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace CleanArchProject.Infra.Data.EntitiesConfiguration
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name).HasMaxLength(100).IsRequired();
+            builder.Property(x => x.Description).HasMaxLength(200).IsRequired();
+            builder.Property(p => p.Price).HasPrecision(10, 2).IsRequired();
+
+            builder.HasOne(e => e.Category).WithMany(e => e.Products).HasForeignKey(e => e.CategoryId);
+        }   
+    }
+}
