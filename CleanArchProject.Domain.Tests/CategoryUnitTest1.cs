aac6547d7b169f52821f7459bf0fc13644fbe6a5
Vinicius Coutinho <vini.coutinho.if@gmail.com>
2022-02-13 21:01:30 -0300

    Domain.Tests: Product and Category Unit tests

---
+++ CleanArchProject.Domain.Tests/CategoryUnitTest1.cs
@@ -0,0 +1,57 @@
+﻿using CleanArchProject.Domain.Entities;
+using CleanArchProject.Domain.Validation;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace CleanArchProject.Domain.Tests
+{
+    public class CategoryTest
+    {
+        [Fact(DisplayName = "New Category With Valid State")]
+        public void NewCategory_WithValidParameters_NotThrowException()
+        {
+            Action action = () => new Category(1, "Category name");
+            action.Should()
+                .NotThrow<DomainExceptionValidation>();
+        }
+
+        [Fact(DisplayName = "Create Category With Negative id Value")]
+        public void NewCategory_WithNegativeIdValue_ThrowInvalidIdException()
+        {
+            Action action = () => new Category(-1, "Category name");
+            action.Should()
+                .Throw<DomainExceptionValidation>()
+                .WithMessage("Invalid Id Value");
+        }
+
+
+        [Fact(DisplayName = "Create Category With short Name")]
+        public void NewCategory_WithShortName_ThrowTooShortException()
+        {
+            Action action = () => new Category(1, "Ca");
+            action.Should()
+                .Throw<DomainExceptionValidation>()
+                .WithMessage("Invalid name, too short, minium 3 characters");
+        }
+
+        [Theory(DisplayName = "Create Category With Null, Empty and White Space Name")]
+        [InlineData("")]
+        [InlineData("   ")]
+        [InlineData(null)]
+        public void CreateCategory_WithNullEmptyOrWithSpaceName_ThrowInvalidName(string name)
+        {
+            Action action = () => new Category(1, name);
+            action.Should()
+                .Throw<DomainExceptionValidation>()
+                .WithMessage("Invalid name. Name is required");
+        }
+        [Fact]
+        public void UpdateCategory_WithValidParam_NotThrowException()
+        {
+            Action action = () => new Category(1, "Category").Update("New Category");
+            action.Should()
+                .NotThrow<DomainExceptionValidation>();
+        }
+    }
+}
---
+++ CleanArchProject.Domain.Tests/ProductUnitTest1.cs
@@ -0,0 +1,101 @@
+﻿using CleanArchProject.Domain.Entities;
+using CleanArchProject.Domain.Validation;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace CleanArchProject.Domain.Tests
+{
+    public class ProductDomainUnitTest1
+    {
+        [Fact]
+        public void NewProduct_WithValidParams_NotThrowException()
+        {
+            Action action = () => new Product(1, "Name", "Description", 100.50m, 5, "image code", 1);
+            action.Should()
+                .NotThrow<DomainExceptionValidation>();
+        }
+
+        [Fact]
+        public void NewProduct_WithInvalidIdParam_ThrowException()
+        {
+            Action action = () => new Product(-1, "Name", "Description", 100.50m, 5, "image code", 1);
+            action.Should()
+                .Throw<DomainExceptionValidation>()
+                .WithMessage("Invalid Id value.");
+        }
+
+        [Theory(DisplayName = "Create Product With Null, Empty and White Space Name")]
+        [InlineData("")]
+        [InlineData("   ")]
+        [InlineData(null)]
+        public void NewProduct_WithNullEmptyOrWithSpaceName_ThrowException(string name)
+        {
+            Action action = () => new Product(1, name, "Description", 100.50m, 5, "image code", 1);
+            action.Should()
+                .Throw<DomainExceptionValidation>()
+                .WithMessage("Invalid name. Name is required");
+        }
+
+        [Fact]
+        public void NewProduct_WithTooShortName_ThrowException()
+        {
+            Action action = () => new Product(1, "Na", "Description", 100.50m, 5, "image code", 1);
+            action.Should()
+                .Throw<DomainExceptionValidation>()
+                .WithMessage("Invalid name, too short, minium 3 characteres");
+        }
+
+        [Fact]
+        public void NewProduct_WithInvalidPrice_ThrowException()
+        {
+            Action action = () => new Product(1, "Name", "Description", -1, 5, "image code", 1);
+            action.Should()
+                .Throw<DomainExceptionValidation>()
+                .WithMessage("Invalid price value");
+        }
+
+        [Fact]
+        public void NewProduct_WithInvalidStock_ThrowException()
+        {
+            Action action = () => new Product(1, "Name", "Description", 10, -1, "image code", 1);
+            action.Should()
+                .Throw<DomainExceptionValidation>()
+                .WithMessage("Invalid stock value");
+        }
+
+        [Fact]
+        public void NewProduct_WithTooLongImageName_ThrowException()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m,
+                99, "product image toooooooooooooooooooooooooooooooooooooooooooo loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooogggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg", 1);
+
+            action.Should()
+                .Throw<DomainExceptionValidation>()
+                .WithMessage("Invalid image name, too long, maximum is 250 characters");
+        }
+
+        [Fact]
+        public void NewProduct_WithNullImageName_NotThrowNullReferenceException()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, null, 2);
+            action.Should().NotThrow<NullReferenceException>();
+        }
+
+        [Fact]
+        public void NewProduct_WithEmptyImageName_NotThrowDomainException()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, "", 2);
+            action.Should().NotThrow<DomainExceptionValidation>();
+        }
+
+        [Fact]
+        public void UpdateCategory_WithValidParam_NotThrowException()
+        {
+            Action action = () => new Product(1, "name", "Description", 100.50m, 5, "image code", 1).Update("name2", "Description2", 100.50m, 5, "image code2", 2);
+            action.Should()
+                .NotThrow<DomainExceptionValidation>();
+        }
+
+    }
+}
